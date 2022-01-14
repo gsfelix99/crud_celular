a885a2af9603e5e7b7a6780b7600ad63c5dfec92
Gabriel Felix <g.felix@estudante.ifmt.edu.br>
2022-01-14 18:19:42 -0400

    Criado a função response_reporte().

--- crud_celular.py
+++ crud_celular.py
@@ -36,3 +36,10 @@ class Celular(db.Model):
             "velocidade_processamento": self.velocidade_processamento,
             "memoria_ram": self.memoria_ram
         }
+
+
+def response_reporte(status, nome_do_conteudo, conteudo, mensagem="False"):
+    body = {nome_do_conteudo: conteudo}
+    if mensagem:
+        body["mensagem"] = mensagem
+    return Response(json.dumps(body), status=status, mimetype="application/json")
